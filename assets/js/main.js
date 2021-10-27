279c079f639602bd3a47d0670e2cb62f380dd26d
“rouzbehmozafari” <rouzbeh.mozafari@yahoo.com>
2021-10-27 16:09:03 +0200

    first

---
+++ assets/js/main.js
@@ -0,0 +1,41 @@
+
+let backc = document.getElementById('backc');
+// let circle = document.getElementById('circle');
+let dd = document.getElementById('dd');
+let dd2 = document.getElementById('dd2');
+let dd3 = document.getElementById('dd3');
+
+
+
+function backgg() {
+    dd.classList.add("col");
+    dd2.classList.add("col");
+    dd3.classList.add("col");
+}
+function backggr() {
+    dd.classList.remove("col");
+    dd2.classList.remove("col");
+    dd3.classList.remove("col");
+}
+
+function circle1() {
+    dd.classList.add("round");
+    dd2.classList.add("round");
+    dd3.classList.add("round");
+}
+function circler() {
+    dd.classList.remove("round");
+    dd2.classList.remove("round");
+    dd3.classList.remove("round");
+}
+
+function backi() {
+    dd.classList.add("backimg");
+    dd2.classList.add("backimg");
+    dd3.classList.add("backimg");
+}
+function backir() {
+    dd.classList.remove("backimg");
+    dd2.classList.remove("backimg");
+    dd3.classList.remove("backimg");
+}
